869c40f64b8eb1430e376ddfbf227fa5c5255802
Adarsh Shrivastav <aadarshshrivastav01@gmail.com>
2026-05-26 09:06:16 +0530

    written app.js & server.js

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import http from "http";
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+import { initSocket } from "./config/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    const server = http.createServer(app);
+
+    initSocket(server);
+
+    server.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Server Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+startServer();
